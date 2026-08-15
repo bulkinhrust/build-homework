ae10a4fafa4f880c3a85238193fc7fb5d52dd5b7
marina_khmeleva <khmeleva@gnivc.ru>
2026-08-15 22:20:30 +0300

    task 4

--- tasks/4/main.js
+++ tasks/4/main.js
@@ -4,12 +4,17 @@ import * as astring from "astring";
 import { parse } from "acorn";
 
 // read file with source code
+const code = fs.readFileSync("./entry.js", "utf-8");
 
 // get ast from source code
+const ast = parse(code, { ecmaVersion: 2020 });
+
+fs.writeFileSync('ast.json', JSON.stringify(ast, null, ' '));
 
 // transform ast
+const transformedAST = transformer(ast);
 
 // convert ast to source code
-
+const result = astring.generate(transformedAST);
 // write source code to file
 fs.writeFileSync("./result.js", result);
