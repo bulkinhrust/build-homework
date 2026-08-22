b8e4337768b90387a0accce7b340a56376e95d16
marina_khmeleva <khmeleva@gnivc.ru>
2026-08-22 10:11:27 +0300

    fix

--- tasks/1/build.sh
+++ tasks/1/build.sh
@@ -4,4 +4,4 @@ echo "Build start"
 rm -rf dist
 mkdir -p dist
 
-cat src/jquery.js src/index.js | tr -d '\t\n' > dist/entry.js
+cat src/jquery.js src/index.js | tr -d '\t\n' | tr -s ' ' > dist/entry.js
